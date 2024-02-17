6cd810d5d4648dbe12700b285a9b6bb4fd92f6c0
Siou <abc8631916@gmail.com>
2024-02-17 11:37:25 +0800

    將上下頁的鍵做成獨立function 可重複使用

---
+++ src/components/Menu/admin/PageButton.js
@@ -0,0 +1,77 @@
+import { Button, FormLabel } from "react-bootstrap"
+import styled from "styled-components"
+import { useState } from "react"
+
+const Form = styled(FormLabel)`
+    margin-right: 2px;
+    margin-top: 5px;
+`
+const Btn = styled(Button)`
+    font-size: 14px;
+    margin-right: 2px;
+
+`
+
+function PageBtn(props) {
+    const { color, nowPage, totalPage, setNowPage } = props
+    const btnAry = ['第一筆', '上一筆', '',  '下一筆', '最後一筆']
+    const st = ['start', 'last', '', 'next', 'end']
+
+    const [disLast, setDisLast] = useState(true)
+    const [disNext, setDisNext] = useState(false)
+
+    const editPage = (st) => {
+        switch(st) {
+            case 'next':
+                let nextPage = nowPage + 1
+                setDisLast(false)
+                setDisNext(nextPage + 1 > totalPage ?true :false)
+                setNowPage(nextPage)
+                break;
+            case 'last':
+                let lastPage = nowPage - 1
+                setDisLast(lastPage === 1 ?true :false)
+                setDisNext(false)
+                setNowPage(lastPage)
+                break;
+            case 'start':
+                setDisLast(true)
+                setDisNext(false)
+                setNowPage(1)
+                break;
+            case 'end':
+                setDisLast(false)
+                setDisNext(true)
+                setNowPage(totalPage)
+                break;
+            default:
+                break;
+        }
+
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth' // 平滑滚动到顶部
+        });
+    }
+
+
+    return (
+        <>
+            {
+                btnAry.map((item, index) => (
+                    index === 2
+                    ?<Form>{nowPage} / {totalPage}</Form>
+                    :<Btn 
+                            key={item}
+                            variant={color} 
+                            disabled={index < 2 ?disLast :disNext} 
+                            onClick={() => { editPage(st[index]) }}
+                        >{item}
+                        </Btn>
+                ))
+            }
+        </>
+    )
+}
+
+export default PageBtn
--- src/components/Menu/admin/read_user.js
+++ src/components/Menu/admin/read_user.js
@@ -7,6 +7,7 @@ import { TopItems } from "../TopItem";
 import Add_user from "./add_user";
 import GetUserInfo from "./getUser";
 import styled from "styled-components";
+import PageBtn from "./PageButton";
 
 const PageDiv = styled.div`
     margin-top: 20px;
@@ -16,12 +17,14 @@ const PageDiv = styled.div`
 
 
 function User_info() {
+    const remainder = 10
+
     let userInfo = JSON.parse(sessionStorage.getItem('userInfo'))
     const token = userInfo['token']
     
     const [resData, setResData] = useState({})  //存放後端傳回資料
     const [totalPage, setTotalPage] = useState(-1)
-    const [pageNow, setPageNow] = useState(1)
+    const [nowPage, setNowPage] = useState(1)
 
     useEffect(() => {  //取得SQL表數量
         const fetchData = async () => {
@@ -30,7 +33,9 @@ function User_info() {
                     empToken: token,
                     tableName: 'empinfo'
                 })
-                setTotalPage(response.data[0].TOTAL_ROWS)
+                let total_rows = response.data[0].TOTAL_ROWS
+                
+                setTotalPage(Math.floor(total_rows / remainder) + 1)
             }catch(err) {
                 console.log(err)
             }
@@ -44,7 +49,7 @@ function User_info() {
             try {
                 const response = await axiosInstance.post(process.env.REACT_APP_GetUserInfo, {
                     empToken: token,
-                    page: pageNow
+                    page: nowPage * remainder - 9
                 })
                 
                 setResData(response.data)
@@ -54,39 +59,9 @@ function User_info() {
         }
 
         fetchData()
-    }, [pageNow])
+    }, [nowPage])
 
-    const [disLast, setDisLast] = useState(true)
-    const [disNext, setDisNext] = useState(false)
 
-    const editPage = (st) => {
-        switch(st) {
-            case 'next':
-                let nextPage = pageNow + 10
-                setDisLast(false)
-                setDisNext(nextPage + 10 >= totalPage ?true :false)
-                setPageNow(nextPage)
-                break;
-            case 'last':
-                let lastPage = pageNow - 10
-                setDisLast(lastPage == 1 ?true :false)
-                setDisNext(false)
-                setPageNow(lastPage)
-                break;
-            case 'start':
-                setDisLast(true)
-                setDisNext(false)
-                setPageNow(1)
-                break;
-            case 'end':
-                let mod = totalPage % 10
-                let diff = mod == 0 ?totalPage - 10 + 1 :totalPage - mod + 1
-                setDisLast(false)
-                setDisNext(true)
-                setPageNow(diff)
-                break;
-        }
-    }
 
 
     return(
@@ -95,11 +70,14 @@ function User_info() {
             <Add_user></Add_user>
             <GetUserInfo resData = { resData } token = { token } />
             <PageDiv>
-                <Button variant="info" disabled={disLast} onClick={() => {editPage('start')}}>第一筆</Button>{' '}
-                <Button variant="info" disabled={disLast} onClick={() => {editPage('last')}}>前一筆</Button>{' '}
-                <Button variant="info" disabled={disNext} onClick={() => {editPage('next')}}>下一筆</Button>{' '}
-                <Button variant="info" disabled={disNext} onClick={() => {editPage('end')}}>最後一筆</Button>{' '}
+                <PageBtn
+                    color='info'
+                    totalPage={totalPage} 
+                    nowPage={nowPage} 
+                    setNowPage={setNowPage} 
+                />
             </PageDiv>
+            
         </>
     )
 }
